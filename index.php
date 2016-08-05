be016cb91198abf0017c2ff1be4dd0a90663c36a
Yavdat92 <yavdat92@mail.ru>
2016-08-05 11:34:40 +0300

    +added bootstrap

--- index.php
+++ index.php
@@ -1,13 +1,3 @@
 <?php
 
-//function __autoload($class)
-//{
-//    require __DIR__."/".str_replace('\\','/',$class).'.php';
-//}
-use App\Models\User;
-require __DIR__.'/autoload.php';
-
-$user=new User();
-$user->name='Peter';
-$user->email='p@petrov.ru';
-$user->insert();
+include __DIR__."/App/templates/index.php";
